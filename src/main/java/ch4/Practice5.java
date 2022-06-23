ddd909badd909eafde3afed4aeb98e73a2f6faeb
xjr7670 <xjr30226@126.com>
2022-06-23 22:42:53 +0800

    Practice 4.5 正多边形面积

--- src/main/java/ch4/Practice5.java
+++ src/main/java/ch4/Practice5.java
@@ -1,2 +1,24 @@
-package ch4;public class Practice5 {
+package ch4;
+
+import java.util.Scanner;
+
+/**
+ * Area of a regular polygon - 正多边形的面积
+ */
+
+public class Practice5 {
+    public static void main(String[] args) {
+        // 1. Prompt user to enter the number of sides and the length of each side
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of sides: ");
+        int sides = input.nextInt();
+        System.out.print("Enter the side: ");
+        float length = input.nextFloat();
+
+        // 2. Compute the area
+        double area = (sides * length * length) / (4 * Math.tan(Math.PI / sides));
+
+        // 3. Display result
+        System.out.printf("The area of the polygon is %f", area);
+    }
 }
