28aa29c5740ce2c5b172a0da8d415b2e22de2c62
xjr7670 <xjr30226@126.com>
2022-06-28 22:35:22 +0800

    给出字符的Unicode码

--- src/main/java/ch4/Practice9.java
+++ src/main/java/ch4/Practice9.java
@@ -1,2 +1,22 @@
-package ch4;public class Practice9 {
+package ch4;
+
+import java.util.Scanner;
+
+/**
+ * Find the Unicode of a character
+ */
+
+public class Practice9 {
+    public static void main(String[] args) {
+        // 1. Obtain user's input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+
+        // 2. Convert to unicode
+
+        // 3. display result
+        System.out.printf("The Unicode for the character %c is %d", ch, (int) ch);
+    }
 }
