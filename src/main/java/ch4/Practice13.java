9f6fc77228f7f7d7151615a76936f4e6e0d61799
xjr7670 <xjr30226@126.com>
2022-06-30 22:30:14 +0800

    判断元音还是辅音

--- src/main/java/ch4/Practice13.java
+++ src/main/java/ch4/Practice13.java
@@ -1,2 +1,27 @@
-package ch4;public class Practice13 {
+package ch4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Vowel or consonant?
+ */
+
+public class Practice13 {
+    public static void main(String[] args) {
+        // 1. Obtain user's input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        String letter = input.nextLine();
+        char ch = letter.toLowerCase(Locale.ROOT).charAt(0);
+
+        // 2. Estimate
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.printf("%s is a vowel\n", letter);
+        } else if (ch > 'a' && ch <= 'z') {
+            System.out.printf("%s is a consonant\n", letter);
+        } else {
+            System.out.printf("%s is an invalid input\n", letter);
+        }
+    }
 }
