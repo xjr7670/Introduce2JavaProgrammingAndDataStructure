af44d43d200d1498d1bab35529f720d5f0c7f613
xjr7670 <xjr30226@126.com>
2022-07-04 22:36:48 +0800

    Process string

--- src/main/java/ch4/Practice20.java
+++ src/main/java/ch4/Practice20.java
@@ -1,2 +1,23 @@
-package ch4;public class Practice20 {
+package ch4;
+
+import java.util.Scanner;
+
+/**
+ * Process a string
+ */
+
+public class Practice20 {
+    public static void main(String[] args) {
+        // 1. Obtain a string
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String s = input.nextLine();
+
+        // 2. Check
+        int len = s.length();
+        char lastCh = s.charAt(len - 1);
+
+        // 3. display result
+        System.out.printf("%s's lenght is %d, it's last character is %c\n", s, len, lastCh);
+    }
 }
