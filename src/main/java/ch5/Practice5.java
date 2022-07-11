5f169fdf25617f0b6d67b2ca429e8083c0a8533a
xjr7670 <xjr30226@126.com>
2022-07-11 22:35:38 +0800

    conversion from celsius from fahrenheit and fahreneit to celsius

--- src/main/java/ch5/Practice5.java
+++ src/main/java/ch5/Practice5.java
@@ -1,2 +1,18 @@
-package ch5;public class Practice5 {
+package ch5;
+
+/**
+ * Conversion from ℃ to ℉ and ℉ to℃
+ */
+
+public class Practice5 {
+    public static void main(String[] args) {
+        // print table title
+        System.out.println("Celsius\tFahrenheit\t|\tFahrenheit\tCelsius");
+        double fahrenheit = 0.0, celsius = 0.0;
+        for (int i = 0, j = 20; i <= 100; i += 2, j += 5) {
+            fahrenheit = i * 9 / 5.0 + 32;
+            celsius = (j - 32) * 5.0 / 9;
+            System.out.printf("%d\t\t%10.3f\t|\t%d\t\t%11.3f\n", i, fahrenheit, j, celsius);
+        }
+    }
 }
