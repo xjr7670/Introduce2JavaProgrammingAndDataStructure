6604f379e7ba86c0b07fb3f4ed027c44d5e7e32d
xjr7670 <xjr30226@126.com>
2022-07-11 22:34:39 +0800

    conversion from inch to centimeter

--- src/main/java/ch5/Practice4.java
+++ src/main/java/ch5/Practice4.java
@@ -1,2 +1,17 @@
-package ch5;public class Practice4 {
+package ch5;
+
+/**
+ * Conversion from inch to centimeter
+ */
+
+public class Practice4 {
+    public static void main(String[] args) {
+        // print title
+        System.out.println("Inches\t\tCentimeters");
+        double centimeters = 0.0;
+        for (int inches = 1; inches <= 10; inches++) {
+            centimeters = inches * 2.54;
+            System.out.printf("%d\t\t%.2f\n", inches, centimeters);
+        }
+    }
 }
