b97bf707452704bb6ede75e049a21c8571fefcb9
xjr7670 <xjr30226@126.com>
2022-07-11 22:36:07 +0800

    Conversion from square meter to ping

--- src/main/java/ch5/Practice6.java
+++ src/main/java/ch5/Practice6.java
@@ -1,2 +1,18 @@
-package ch5;public class Practice6 {
+package ch5;
+
+/**
+ * Conversion from square meter to ping
+ */
+
+public class Practice6 {
+    public static void main(String[] args) {
+        // print table title
+        System.out.println("Ping\tSquare meter\t|\tSquare meter\tPing");
+        double squareMeter = 0.0, ping = 0.0;
+        for (int p = 10, s = 30; p <= 80; p += 5, s += 5) {
+            squareMeter = p * 3.305;
+            ping = s / 3.305;
+            System.out.printf("%d\t\t%12.3f\t|\t%d\t\t%12.3f\n", p, squareMeter, s, ping);
+        }
+    }
 }
