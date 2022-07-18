7442b22927c8990c0e182b44240912eadebd6962
xjr7670 <xjr30226@126.com>
2022-07-18 23:48:44 +0800

    find numbers divisible by 3 and 4

--- src/main/java/ch5/Practice10.java
+++ src/main/java/ch5/Practice10.java
@@ -1,2 +1,25 @@
-package ch5;public class Practice10 {
+package ch5;
+
+/**
+ * Find numbers divisible by 3 and 4
+ */
+
+public class Practice10 {
+    public static void main(String[] args) {
+        // some const variable
+        final int LINE_LENGTH = 10;
+        int count = 0;
+        for (int i = 100; i <= 1000; i++) {
+            if (i % 12 == 0) {
+                System.out.print(i);
+                ++count;
+            }
+            if (count == 10) {
+                System.out.println();
+                count = 0;
+            } else {
+                System.out.print(" ");
+            }
+        }
+    }
 }
