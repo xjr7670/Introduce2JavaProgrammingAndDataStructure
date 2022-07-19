43c2fcec28bf080ea449e3d35f95d2ef7fce7fab
xjr7670 <xjr30226@126.com>
2022-07-19 22:33:57 +0800

    find numbers divisible by 3 or 4 but not both

--- src/main/java/ch5/Practice11.java
+++ src/main/java/ch5/Practice11.java
@@ -1,2 +1,27 @@
-package ch5;public class Practice11 {
+package ch5;
+
+/**
+ * Find numbers divisible by 3 or 4, but not both
+ */
+
+public class Practice11 {
+    public static void main(String[] args) {
+        final int LING_LENGTH = 10;
+        int count = 0;
+
+        System.out.println("The numbers can divisible by 3 or 4 but not both are: ");
+        for (int i = 100; i <= 200; i++) {
+            if ((i % 3 == 0 || i % 4 == 0) && (i % 12 != 0)) {
+                System.out.print(i);
+                ++count;
+                if (count == LING_LENGTH) {
+                    System.out.println();
+                    count = 0;
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+        }
+    }
 }
