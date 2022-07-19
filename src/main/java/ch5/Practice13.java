fbb8aca1072b31156252c08b489a7e787316e23c
xjr7670 <xjr30226@126.com>
2022-07-19 22:34:43 +0800

    find the largest n that pow(n, 2) < 12,000

--- src/main/java/ch5/Practice13.java
+++ src/main/java/ch5/Practice13.java
@@ -1,2 +1,17 @@
-package ch5;public class Practice13 {
+package ch5;
+
+/**
+ * Find the largest n such that pow(n, 2) < 12,000
+ */
+
+public class Practice13 {
+    public static void main(String[] args) {
+        System.out.println("The largest n that pow(n, 2) < 12,000 is: ");
+        int nPow2 = 0, n = 0;
+
+        while (Math.pow(n, 2) < 12000) {
+            ++n;
+        }
+        System.out.println(n);
+    }
 }
