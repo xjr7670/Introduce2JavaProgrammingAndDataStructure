6645d82ab35e4b23d64d553ef1da6ea266b2d783
xjr7670 <xjr30226@126.com>
2022-07-19 22:34:26 +0800

    find the smallest n that pow(n, 3) > 12,000

--- src/main/java/ch5/Practice12.java
+++ src/main/java/ch5/Practice12.java
@@ -1,2 +1,18 @@
-package ch5;public class Practice12 {
+package ch5;
+
+/**
+ * Find the smallest n such that pow(n, 3) > 12,000
+ */
+
+public class Practice12 {
+    public static void main(String[] args) {
+        System.out.println("The smallest n that pow(n, 3) > 12,000 is :");
+        int nPow3 = 0, n = 0;
+
+        while (Math.pow(n, 3) <= 12000) {
+            ++n;
+        }
+
+        System.out.println(n);
+    }
 }
