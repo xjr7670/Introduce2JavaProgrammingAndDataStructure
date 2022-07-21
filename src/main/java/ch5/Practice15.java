ba1e5d4a4521486c770baf5d18cfde0e6c19b431
xjr7670 <xjr30226@126.com>
2022-07-21 22:57:03 +0800

    display the ASCII character table

--- src/main/java/ch5/Practice15.java
+++ src/main/java/ch5/Practice15.java
@@ -1,2 +1,23 @@
-package ch5;public class Practice15 {
+package ch5;
+
+/**
+ * Display the ASCII character table
+ */
+
+public class Practice15 {
+    public static void main(String[] args) {
+
+        final int LINE_LENGTH = 10;
+        int count = 0;
+
+        for (int i = 33; i <= 126; i++) {
+            System.out.print((char) i);
+            if (++count == LINE_LENGTH) {
+                System.out.println();
+                count = 0;
+            } else {
+                System.out.print(" ");
+            }
+        }
+    }
 }
