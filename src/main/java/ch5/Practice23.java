f29704e8bf530fd2d0e597677680c824e995e368
xjr7670 <xjr30226@126.com>
2022-08-01 23:23:28 +0800

    Demonstrate cancellation errors

--- src/main/java/ch5/Practice23.java
+++ src/main/java/ch5/Practice23.java
@@ -1,2 +1,26 @@
-package ch5;public class Practice23 {
+package ch5;
+
+/**
+ * Demonstrate cancellation errors
+ */
+
+public class Practice23 {
+    public static void main(String[] args) {
+        double sum = 0.0;
+        int left = 1;
+        int right = 50000;
+        // from left to right
+        while (left <= right) {
+            sum += 1.0 / left;
+            ++left;
+        }
+        System.out.printf("Summation of left to right: %.10f\n", sum);
+        sum = 0.0;
+        left = 1;
+        while (right >= left) {
+            sum += 1.0 / right;
+            --right;
+        }
+        System.out.printf("Summation of right to left: %.10f\n", sum);
+    }
 }
