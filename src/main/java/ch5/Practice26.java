95ca65c6c134330ede35d08fd1878b935f25f08a
xjr7670 <xjr30226@126.com>
2022-08-04 21:04:20 +0800

    compute e

--- src/main/java/ch5/Practice26.java
+++ src/main/java/ch5/Practice26.java
@@ -1,2 +1,18 @@
-package ch5;public class Practice26 {
+package ch5;
+
+/**
+ * Compute e
+ */
+
+public class Practice26 {
+    public static void main(String[] args) {
+        double e = 1.0, item = 1.0;
+        int times = 100000;
+        e += item;
+        for (int i = 2; i <= times; i++) {
+            item /= i;
+            e += item;
+        }
+        System.out.printf("i = %d, e = %f\n", times, e);
+    }
 }
