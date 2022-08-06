2cb9e0161c62de0f9a850fee71d6bb01892bbab7
xjr7670 <xjr30226@126.com>
2022-08-06 11:28:58 +0800

    Game: lottery

--- src/main/java/ch5/Practice32.java
+++ src/main/java/ch5/Practice32.java
@@ -1,2 +1,45 @@
-package ch5;public class Practice32 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Game: lottery
+ */
+
+public class Practice32 {
+    public static void main(String[] args) {
+        // 1. Generate two digit's number
+        int lotteryDigit1 = (int)(Math.random() * 9), lotteryDigit2;
+
+        while (true) {
+            lotteryDigit2 = (int)(Math.random() * 9);
+            if (lotteryDigit2 != lotteryDigit1) {
+                break;
+            }
+        }
+
+        // Prompt the user to enter a guess
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your lottery pick (tow digits): ");
+
+        int guess = input.nextInt();
+
+        // Get digits from guess
+        int guessDigit1 = guess / 10;
+        int guessDigit2 = guess % 10;
+
+        System.out.printf("The lottery number is %d%d\n", lotteryDigit1, lotteryDigit2);
+
+        // Check the guess
+        if (guess == (lotteryDigit1 * 10 + lotteryDigit2)) {
+            System.out.println("Exact match: you win $10.000");
+        } else if (guessDigit2 == lotteryDigit1 && guessDigit1 == lotteryDigit2) {
+            System.out.println("Match all digits: you win $3,000");
+        } else if (guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2
+                || guessDigit2 == lotteryDigit1 || guessDigit2 == lotteryDigit2) {
+            System.out.println("Match one digit: you win $1,000");
+        } else {
+            System.out.println("Sorry, no match");
+        }
+    }
 }
