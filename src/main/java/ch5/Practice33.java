36e4c2f97a31f24c47aecc535103eb395b3a027e
xjr7670 <xjr30226@126.com>
2022-08-06 11:29:12 +0800

    perfect number

--- src/main/java/ch5/Practice33.java
+++ src/main/java/ch5/Practice33.java
@@ -1,2 +1,24 @@
-package ch5;public class Practice33 {
+package ch5;
+
+/**
+ * Perfect number
+ */
+
+public class Practice33 {
+    public static void main(String[] args) {
+        int divisorsSum, divisor;
+        for (int i = 1; i < 10000; i++) {
+            divisor = 1;
+            divisorsSum = 0;
+            while (divisor <= i / 2) {
+                if (i % divisor == 0) {
+                    divisorsSum += divisor;
+                }
+                ++divisor;
+            }
+            if (divisorsSum == i) {
+                System.out.printf("%d\t", i);
+            }
+        }
+    }
 }
