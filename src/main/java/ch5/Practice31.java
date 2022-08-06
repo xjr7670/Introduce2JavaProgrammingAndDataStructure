2f49f4bcf639271bf112c63d7ee29436c0e9cdd3
xjr7670 <xjr30226@126.com>
2022-08-06 11:28:41 +0800

    Financial application: compute CD value

--- src/main/java/ch5/Practice31.java
+++ src/main/java/ch5/Practice31.java
@@ -1,2 +1,29 @@
-package ch5;public class Practice31 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Financial application: compute CD value
+ */
+
+public class Practice31 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the initial deposit amount: ");
+        double deposit = input.nextDouble();
+        System.out.print("Enter annual percentage yield: ");
+        double percentage = input.nextDouble();
+        System.out.print("Enter maturity period (number of months):");
+        int period = input.nextInt();
+
+        // 2. Computer the result
+        double cdValue = deposit;
+        double monthlyRate = percentage / 12 / 100;
+        System.out.printf("%5s\t%10s\n", "Month", "CD Value");
+        for (int i = 1; i <= period; i++) {
+            cdValue = cdValue + cdValue * monthlyRate;
+            System.out.printf("%-5d\t%10.2f\n", i, cdValue);
+        }
+
+    }
 }
