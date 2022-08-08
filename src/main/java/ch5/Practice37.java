cf158823942b6d8cf2dbfbb597e8d745bc055ddc
xjr7670 <xjr30226@126.com>
2022-08-08 22:44:44 +0800

    Practice 5.37 - Decimal to binary

--- src/main/java/ch5/Practice37.java
+++ src/main/java/ch5/Practice37.java
@@ -1,2 +1,33 @@
-package ch5;public class Practice37 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Decimal to binary
+ */
+
+public class Practice37 {
+    public static void main(String[] args) {
+        // 1. get user's input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        int number = input.nextInt();
+        int temp = number;
+        // 2. Convert to binary
+        int y = number;
+        String result = "";
+        while (number > 1) {
+            y = number % 2;
+            result += y;
+            number /= 2;
+        }
+
+        result += number;
+
+        System.out.printf("%d convert to binary is: ", temp);
+        for (int i = result.length() - 1; i >= 0; i--) {
+            System.out.print(result.charAt(i));
+        }
+        System.out.println();
+    }
 }
