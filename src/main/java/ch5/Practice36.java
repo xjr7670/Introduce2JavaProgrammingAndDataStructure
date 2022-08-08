9eecfa12dd78b2be7d12a65bcfdff8cbc176fc74
xjr7670 <xjr30226@126.com>
2022-08-08 22:44:18 +0800

    checking ISBN

--- src/main/java/ch5/Practice36.java
+++ src/main/java/ch5/Practice36.java
@@ -1,2 +1,32 @@
-package ch5;public class Practice36 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Business application: checking ISBN
+ */
+
+public class Practice36 {
+    public static void main(String[] args) {
+        // get user input
+        System.out.println("Enter the first 9 digits of an ISBN as integer: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int temp = number;
+        int n9 = 0;
+        System.out.print(n9);
+        for (int i = 9; i >= 2; i--) {
+            n9 += number % 10 * i;
+            number /= 10;
+        }
+
+        n9 %= 11;
+
+        System.out.print(temp);
+        if (n9 == 10) {
+            System.out.println("X");
+        } else {
+            System.out.println(n9);
+        }
+    }
 }
