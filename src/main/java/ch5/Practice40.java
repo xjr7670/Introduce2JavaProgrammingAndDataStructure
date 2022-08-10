11925679b7f23aed9c3c9c4c7d4d336731e46dc3
xjr7670 <xjr30226@126.com>
2022-08-10 22:16:08 +0800

    heads and tails

--- src/main/java/ch5/Practice40.java
+++ src/main/java/ch5/Practice40.java
@@ -1,2 +1,22 @@
-package ch5;public class Practice40 {
+package ch5;
+
+/**
+ * Simulation: heads or tails
+ */
+
+public class Practice40 {
+    public static void main(String[] args) {
+        int heads = 0, tails = 0;
+        int times = 2000000;
+        int temp = times;
+        while (times-- > 0) {
+            if (Math.random() >= 0.5) {
+                ++heads;
+            } else {
+                ++tails;
+            }
+        }
+
+        System.out.printf("heads count: %d, tails count %d\nThe head rate is %.5f\nThe tail rate is %.5f\n", heads, tails, heads * 1.0 / temp, tails * 1.0 / temp);
+    }
 }
