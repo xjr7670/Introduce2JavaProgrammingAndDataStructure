c2ade509f6cca993edde0e8aee76755fd4e06bee
xjr7670 <xjr30226@126.com>
2022-08-10 22:16:27 +0800

    occurrence of max numbers

--- src/main/java/ch5/Practice41.java
+++ src/main/java/ch5/Practice41.java
@@ -1,2 +1,31 @@
-package ch5;public class Practice41 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Occurrence of max numbers
+ */
+
+public class Practice41 {
+    public static void main(String[] args) {
+        // 1. Get user's input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter numbers: ");
+        int num;
+        int max = -10, count = 0;
+        while (true) {
+            num = input.nextInt();
+            if (num > max) {
+                max = num;
+                count = 1;
+            } else if (num == max) {
+                ++count;
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        System.out.printf("The largest number is: %d\n", max);
+        System.out.printf("The occurrence count of the largest number is %d\n", count);
+    }
 }
