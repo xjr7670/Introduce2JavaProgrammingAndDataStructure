d18e5c0f0857df4711ee1a2814b228b9b77e6370
xjr7670 <xjr30226@126.com>
2022-08-11 22:49:02 +0800

    bit-level operations

--- src/main/java/ch5/Practice44.java
+++ src/main/java/ch5/Practice44.java
@@ -1,2 +1,36 @@
-package ch5;public class Practice44 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Computer architecture: bit-level operations
+ */
+
+public class Practice44 {
+    public static void main(String[] args) {
+        // 1. Get user's input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = input.nextInt();
+
+        // 2. Convert with 8 bits
+        String result = "";
+        int upper;
+        if (num < 256) {
+            upper = 128;
+        } else {
+            upper = 32768;
+        }
+        int i = upper;
+        while (i > 0) {
+            if ((num & i) != 0) {
+                result += "1";
+            } else {
+                result += "0";
+            }
+            i = i >> 1;
+        }
+
+        System.out.println("The 8 bits are: " + result);
+    }
 }
