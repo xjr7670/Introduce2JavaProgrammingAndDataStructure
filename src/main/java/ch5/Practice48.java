618e4c46e9ac6e9cfeaa890672afe10843da02fd
xjr7670 <xjr30226@126.com>
2022-08-13 16:29:06 +0800

    process string

--- src/main/java/ch5/Practice48.java
+++ src/main/java/ch5/Practice48.java
@@ -1,2 +1,24 @@
-package ch5;public class Practice48 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Process string
+ */
+
+public class Practice48 {
+    public static void main(String[] args) {
+        System.out.print("Enter a string: ");
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if (i % 2 != 0 && c != ' ') {
+                System.out.print(c);
+            }
+        }
+        System.out.println();
+    }
 }
