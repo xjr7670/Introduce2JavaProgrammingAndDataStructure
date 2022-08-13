f389cdc3878a82dab9c89235ba38fbdbf88d51e7
xjr7670 <xjr30226@126.com>
2022-08-13 16:29:42 +0800

    Reverse a string

--- src/main/java/ch5/Practice46.java
+++ src/main/java/ch5/Practice46.java
@@ -1,2 +1,20 @@
-package ch5;public class Practice46 {
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Reverse a string
+ */
+
+public class Practice46 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
+
+        System.out.print("The reversed string is: ");
+        for (int i = s.length()-1; i >= 0; i--) {
+            System.out.print(s.charAt(i));
+        }
+    }
 }
