d020dd13dfd5bb9a6e4e0786dffa233d79996eba
xjr7670 <xjr30226@126.com>
2022-08-14 11:04:41 +0800

    longest common prefix

---
+++ src/main/java/ch5/Practice51.java
@@ -0,0 +1,35 @@
+package ch5;
+
+import java.util.Scanner;
+
+/**
+ * Longest common prefix
+ */
+
+public class Practice51 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String s1 = input.nextLine();
+        System.out.print("Enter the second string: ");
+        String s2 = input.nextLine();
+
+        boolean hasCommonPrefix = false;
+
+        String commonPrefix = "";
+        for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                commonPrefix += String.valueOf(s1.charAt(i));
+                hasCommonPrefix = true;
+            } else if (i == 0) {
+                break;
+            }
+        }
+
+        if (hasCommonPrefix) {
+            System.out.printf("The common prefix is %s\n", commonPrefix);
+        } else {
+            System.out.printf("%s and %s has no common prefix.\n", s1, s2);
+        }
+    }
+}
