b7271f622d8becab74b08ccc99bb7ab83b93ece9
xjr7670 <xjr30226@126.com>
2022-08-14 11:09:42 +0800

    two different method to imple

--- src/main/java/ch5/Practice51.java
+++ src/main/java/ch5/Practice51.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 
 public class Practice51 {
     public static void main(String[] args) {
+        method2();
+    }
+
+    public static void method1() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the first string: ");
         String s1 = input.nextLine();
@@ -28,6 +32,27 @@ public class Practice51 {
 
         if (hasCommonPrefix) {
             System.out.printf("The common prefix is %s\n", commonPrefix);
+        } else {
+            System.out.printf("%s and %s has no common prefix.\n", s1, s2);
+        }
+    }
+
+    public static void method2() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String s1 = input.nextLine();
+        System.out.print("Enter the second string: ");
+        String s2 = input.nextLine();
+
+        int i;
+        for (i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                break;
+            }
+        }
+
+        if ( i != 0) {
+            System.out.printf("The common prefix is %s\n", s1.substring(0, i));
         } else {
             System.out.printf("%s and %s has no common prefix.\n", s1, s2);
         }
