183a3dea9abb3538363ee80456d5c3b1221b2158
xjr7670 <xjr30226@126.com>
2022-08-16 22:21:24 +0800

    sum the digits in an integer

--- src/main/java/ch6/Practice2.java
+++ src/main/java/ch6/Practice2.java
@@ -1,2 +1,17 @@
-package ch6;public class Practice2 {
+package ch6;
+
+/**
+ * Sum the digits in an integer
+ */
+
+public class Practice2 {
+    public static int sumDigits(long n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = n / 10;
+        }
+
+        return sum;
+    }
 }
--- src/main/java/ch6/TestPractice2.java
+++ src/main/java/ch6/TestPractice2.java
@@ -1,2 +1,12 @@
-package ch6;public class TestPractice2 {
+package ch6;
+
+import java.util.Scanner;
+
+public class TestPractice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        long n = input.nextLong();
+        System.out.printf("The sum of all digits in this number is: %d\n", Practice2.sumDigits(n));
+    }
 }
