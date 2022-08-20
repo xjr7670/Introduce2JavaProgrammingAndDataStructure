5c5b1c0e86ac610737fd43c2b194409b457104ff
xjr7670 <xjr30226@126.com>
2022-08-20 13:45:37 +0800

    Display an integer reversed

--- src/main/java/ch6/Practice4.java
+++ src/main/java/ch6/Practice4.java
@@ -1,2 +1,14 @@
-package ch6;public class practice4 {
+package ch6;
+
+/**
+ * Display an integer reversed
+ */
+
+public class Practice4 {
+    public static void reverse(int number) {
+        while (number > 0) {
+            System.out.print(number % 10);
+            number /= 10;
+        }
+    }
 }
--- src/main/java/ch6/TestPractice4.java
+++ src/main/java/ch6/TestPractice4.java
@@ -1,2 +1,13 @@
-package ch6;public class TestPractice4 {
+package ch6;
+
+import java.util.Scanner;
+
+public class TestPractice4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        System.out.printf("The reversed number of %d is :", num);
+        Practice4.reverse(num);
+    }
 }
