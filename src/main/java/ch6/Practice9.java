d7167c1e8b0dc87aa8e56b6a118ee598b1140b63
xjr7670 <xjr30226@126.com>
2022-08-23 23:25:04 +0800

    Conversions between pounds and kilograms

--- src/main/java/ch6/Practice9.java
+++ src/main/java/ch6/Practice9.java
@@ -1,2 +1,17 @@
-package ch6;public class Practice9 {
+package ch6;
+
+/**
+ * Conversions between pounds and kilograms
+ */
+
+public class Practice9 {
+    /** Convert from pounds to kilograms */
+    public static double poundToKilogram(double pound) {
+        return pound * 0.453;
+    }
+
+    /** Convert from kilograms to pounds */
+    public static double kilogramToPound(double kilogram) {
+        return kilogram * 2.204;
+    }
 }
--- src/main/java/ch6/TestPractice9.java
+++ src/main/java/ch6/TestPractice9.java
@@ -1,2 +1,14 @@
-package ch6;public class TestPractice9 {
+package ch6;
+
+public class TestPractice9 {
+    public static void main(String[] args) {
+        double pounds, kilograms, p;
+        System.out.printf("%9s\t%6s\t|\t%6s\t%9s\n", "Kilograms", "Pounds", "Pounds", "Kilograms");
+        for (int k = 1; k < 200; k += 2) {
+            p = 17.5 + k * 2.5;
+            pounds = Practice9.kilogramToPound(k);
+            kilograms = Practice9.poundToKilogram(p);
+            System.out.printf("%-9d\t%-6.1f\t|\t%-6.0f\t%-9.2f\n", k, pounds, p, kilograms);
+        }
+    }
 }
