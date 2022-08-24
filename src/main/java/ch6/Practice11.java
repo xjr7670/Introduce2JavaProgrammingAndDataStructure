82c7db7e85446caef4468f751ab06cd76e28367c
xjr7670 <xjr30226@126.com>
2022-08-24 22:58:02 +0800

    compute commissions

--- src/main/java/ch6/Practice11.java
+++ src/main/java/ch6/Practice11.java
@@ -1,2 +1,20 @@
-package ch6;public class Practice11 {
+package ch6;
+
+/**
+ * Financial application: compute commissions
+ */
+
+public class Practice11 {
+    public static double computeCommission(double salesAmount) {
+        double ret;
+        if (salesAmount <= 5000) {
+            ret = salesAmount * 0.08;
+        } else if (salesAmount > 5000 && salesAmount <= 10000) {
+            ret = 400 + (salesAmount - 5000) * 0.1;
+        } else {
+            ret = 900 + (salesAmount - 10000) * 0.12;
+        }
+
+        return ret;
+    }
 }
--- src/main/java/ch6/TestPractice11.java
+++ src/main/java/ch6/TestPractice11.java
@@ -1,2 +1,13 @@
-package ch6;public class TestPractice11 {
+package ch6;
+
+public class TestPractice11 {
+    public static void main(String[] args) {
+        System.out.println("Sales Amount\tCommission");
+        System.out.println("--------------------------");
+        double commission;
+        for (int amount = 10000; amount <= 100000; amount += 5000) {
+            commission = Practice11.computeCommission(amount);
+            System.out.printf("%-12d\t%10.1f\n", amount, commission);
+        }
+    }
 }
