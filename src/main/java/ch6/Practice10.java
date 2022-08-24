a8114161b21bfa2ec2e1c06e5376d26e3bbd7f39
xjr7670 <xjr30226@126.com>
2022-08-24 22:58:24 +0800

    Use the isPrime method

--- src/main/java/ch6/Practice10.java
+++ src/main/java/ch6/Practice10.java
@@ -1,2 +1,25 @@
-package ch6;public class Practice10 {
+package ch6;
+
+/**
+ * Use the isPrime Method
+ */
+
+public class Practice10 {
+    /** Check whether number is prime */
+    public static boolean isPrime(int number) {
+        for (int divisor = 2; divisor <= number / 2; divisor++) {
+            if (number % divisor == 0)  {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int n = 1; n < 10000; n++) {
+            if (isPrime(n)) {
+                System.out.println(n);
+            }
+        }
+    }
 }
