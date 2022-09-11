669687362dc1b2d7ae50570f363efb28ea8613d9
xjr7670 <xjr30226@126.com>
2022-09-11 18:18:11 +0800

    mersenne prime

--- src/main/java/ch6/Practice28.java
+++ src/main/java/ch6/Practice28.java
@@ -8,4 +8,16 @@ public class Practice28 {
     public static long mersennePrime(int p) {
         return Math.round(Math.pow(2, p) - 1);
     }
+
+    public static boolean isPrime(int number) {
+        boolean flag = true;
+        int end = (int) Math.pow(number, 0.5);
+        for (int i = 2; i <= end; i++) {
+            if (number % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
 }
